fadcb5b2d6683318af55e0e3dd916c17936ed79d
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-12-23 12:28:25 +0900

    07 importとexportでモジュールを使いこなそう

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import Article from './Article'
+import Foge from './components/Foge'
+import * as FooBar from './components/FooBar'
 
 class Blog extends Component {
   constructor(props) {
@@ -48,6 +50,10 @@ class Blog extends Component {
           toggle={() => this.togglePublished()}
           count={count}
         />
+        <FooBar.Foo />
+        <FooBar.Bar />
+        <Foge />
+        <button onClick={() => FooBar.Open()}>Click</button>
       </Fragment>
     )
   }
